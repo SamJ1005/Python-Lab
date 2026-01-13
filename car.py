999559eab02dcb9ce675b25dca3b4c09f50e3319
SamJ1005 <samjac7755@gmail.com>
2026-01-13 21:23:13 +0530

    New python files

---
+++ car.py
@@ -0,0 +1,29 @@
+class Car:
+    def __init__(self, make, model, year, price):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.price = price
+
+    def display(self):
+        print(self.year, self.make, self.model, "- Price:", self.price)
+
+
+n = int(input("How many cars want to enter? "))
+cars = []
+
+for i in range(n):
+    print(f"\nEnter details of Car {i+1}:")
+    make = input("Enter Make/Brand: ")
+    model = input("Enter Model: ")
+    year = int(input("Enter Year: "))
+    price = float(input("Enter Price: "))
+    
+    cars.append(Car(make, model, year, price))
+
+limit = float(input("\nEnter the starting price: "))
+print(f"\nCars with price greater than {limit} is:\n")
+
+for c in cars:
+    if c.price > limit:
+        c.display()
---
+++ library.py
@@ -0,0 +1,26 @@
+class Book:
+    def __init__(self, title, author, price):
+        self.title = title
+        self.author = author
+        self.price = price
+
+    def show(self):
+        print("\nTitle  :", self.title)
+        print("Author :", self.author)
+        print("Price  :", self.price)
+        print("-" * 20)
+
+n = int(input("How many books do you want to enter? "))
+books = []
+
+for i in range(n):
+    print(f"\nEnter details of Book {i+1}:")
+    title = input("Enter title: ")
+    author = input("Enter author: ")
+    price = float(input("Enter price: "))
+
+    books.append(Book(title, author, price))
+
+print("\n--- Book Details ---")
+for b in books:
+    b.show()

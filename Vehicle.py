ea3cfd2b3e61040af85b7bd16e89b223d5162a2b
samjac7755@gmail.com <your@email.com>
2026-01-28 21:09:12 +0530

    Updated Vehicle prog

--- Vehicle.py
+++ Vehicle.py
@@ -3,47 +3,49 @@ class Vehicle:
         self.speed = speed
         self.fuel = fuel
 
-    def display_vehicle(self):
+    def display_details(self):
         print("Speed:", self.speed, "km/h")
-        print("Fuel:", self.fuel)
+        print("Fuel Type:", self.fuel)
 
 class Car(Vehicle):
-    def car_type(self):
+    def car_feature(self):
         print("Vehicle Type: Car")
+        print("Feature: Air Conditioning")
 
 class Motorcycle(Vehicle):
     def wheel_type(self):
+        print("Vehicle Type: Motorcycle")
         print("Wheel Type: Two Wheeler")
 
 class Truck(Vehicle):
-    def cargo_capacity(self, capacity):
+    def cargo_capacity(self):
+        capacity = float(input("Enter cargo capacity (in tons): "))
+        print("Vehicle Type: Truck")
         print("Cargo Capacity:", capacity, "tons")
 
 speed = int(input("Enter vehicle speed (km/h): "))
 fuel = input("Enter fuel type: ")
 
-print("\nChoose Vehicle Type")
+print("\nSelect Vehicle")
 print("1. Car")
 print("2. Motorcycle")
 print("3. Truck")
 
 choice = int(input("Enter your choice: "))
-
 if choice == 1:
-    v = Car(speed, fuel)
-    v.display_vehicle()
-    v.car_type()
+    vehicle = Car(speed, fuel)
+    vehicle.display_details()
+    vehicle.car_feature()
 
 elif choice == 2:
-    v = Motorcycle(speed, fuel)
-    v.display_vehicle()
-    v.wheel_type()
+    vehicle = Motorcycle(speed, fuel)
+    vehicle.display_details()
+    vehicle.wheel_type()
 
 elif choice == 3:
-    capacity = float(input("Enter cargo capacity (tons): "))
-    v = Truck(speed, fuel)
-    v.display_vehicle()
-    v.cargo_capacity(capacity)
+    vehicle = Truck(speed, fuel)
+    vehicle.display_details()
+    vehicle.cargo_capacity()
 
 else:
     print("Invalid choice")

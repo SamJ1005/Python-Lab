3f102901995a70d4220d8dae664141410bf259d2
SamJ1005 <196654975+SamJ1005@users.noreply.github.com>
2026-02-18 21:35:14 +0530

    two operover

--- operatorover.py
+++ operatorover.py
@@ -1,4 +1,40 @@
+# class Complex:
+#     def __init__(self, a, b):
+#         self.a = a
+#         self.b = b
+
+#     def __add__(self, other):
+#         return Complex(self.a + other.a, self.b + other.b)
+
+#     def __sub__(self, other):
+#         return Complex(self.a - other.a, self.b - other.b)
+
+#     def display(self):
+#         print(self.a, "+", self.b, "i")
+
+
+# print("Enter First Complex Number")
+# r1 = int(input("a part: "))
+# i1 = int(input("binary part: "))
+
+# print("Enter Second Complex Number")
+# r2 = int(input("a part: "))
+# i2 = int(input("binary part: "))
+
+# c1 = Complex(r1, i1)
+# c2 = Complex(r2, i2)
+
+# add_result = c1 + c2
+# sub_result = c1 - c2
+
+# print("\nAddition Result:")
+# add_result.display()
+
+# print("Subtraction Result:")
+# sub_result.display()
+
 class Complex:
+
     def __init__(self, r, i):
         self.r = r
         self.i = i

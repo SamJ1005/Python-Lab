241e148a0d2e0216d62396b7b80af0404f688844
SamJ1005 <196654975+SamJ1005@users.noreply.github.com>
2026-02-18 20:24:42 +0530

    Updated temperature file

--- temperature.py
+++ temperature.py
@@ -2,18 +2,9 @@ from tempe_package.ctof import cel_to_fah
 from tempe_package.ftoc import fah_to_cel
 
 print("Temperature Conversion Package")
-print("1. Celsius to Fahrenheit")
-print("2. Fahrenheit to Celsius")
 
-choice = int(input("Enter your choice: "))
+c = float(input("\nEnter temperature in Celsius: "))
+print("Fahrenheit:", cel_to_fah(c))
 
-if choice == 1:
-    c = float(input("Enter temperature in Celsius: "))
-    print("Fahrenheit:", cel_to_fah(c))
-
-elif choice == 2:
-    f = float(input("Enter temperature in Fahrenheit: "))
-    print("Celsius:", fah_to_cel(f))
-
-else:
-    print("Invalid choice")
+f = float(input("Enter temperature in Fahrenheit: "))
+print("Celsius:", fah_to_cel(f))

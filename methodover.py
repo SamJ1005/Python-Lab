e2fa23aeca4c4075f5b7eab928eaa5959ec8a961
SamJ1005 <196654975+SamJ1005@users.noreply.github.com>
2026-02-18 20:49:02 +0530

    Updated method overloading

--- methodover.py
+++ methodover.py
@@ -16,3 +16,30 @@ for i in range(n):
 
 result = obj.add(*values)
 print("Sum =", result)
+
+# class MathOperations:
+
+#     def operate(self, operator, *numbers):
+#         if operator == "+":
+#             result = 0
+#             for i in numbers:
+#                 result += i
+#             return result
+        
+#         elif operator == "*":
+#             result = 1
+#             for i in numbers:
+#                 result *= i
+#             return result
+
+
+# obj = MathOperations()
+
+# op = input("Enter operation (+ or *): ")
+# n = int(input("How many numbers? "))
+
+# values = []
+# for i in range(n):
+#     values.append(int(input("Enter number: ")))
+
+# print("Result =", obj.operate(op, *values))

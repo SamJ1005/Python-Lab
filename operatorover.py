897ea4814e24d88f1c610fb1b80316b7df57d8ec
Shyam <196654975+SamJ1005@users.noreply.github.com>
2026-02-19 10:26:18 +0530

    Refactor Complex class and clean up code
    Refactor Complex class and remove commented code.

--- operatorover.py
+++ operatorover.py
@@ -1,38 +1,3 @@
-# class Complex:
-#     def __init__(self, a, b):
-#         self.a = a
-#         self.b = b
-
-#     def __add__(self, other):
-#         return Complex(self.a + other.a, self.b + other.b)
-
-#     def __sub__(self, other):
-#         return Complex(self.a - other.a, self.b - other.b)
-
-#     def display(self):
-#         print(self.a, "+", self.b, "i")
-
-
-# print("Enter First Complex Number")
-# r1 = int(input("a part: "))
-# i1 = int(input("binary part: "))
-
-# print("Enter Second Complex Number")
-# r2 = int(input("a part: "))
-# i2 = int(input("binary part: "))
-
-# c1 = Complex(r1, i1)
-# c2 = Complex(r2, i2)
-
-# add_result = c1 + c2
-# sub_result = c1 - c2
-
-# print("\nAddition Result:")
-# add_result.display()
-
-# print("Subtraction Result:")
-# sub_result.display()
-
 class Complex:
 
     def __init__(self, r, i):
@@ -46,8 +11,10 @@ class Complex:
         return Complex(self.r - other.r, self.i - other.i)
 
     def __str__(self):
-        return f"{self.r} + {self.i}i"
-
+        if self.imag >= 0:
+            return f"{self.real} + {self.imag}i"
+        else:
+            return f"{self.real} - {self.imag}i"
 
 r1 = int(input("Enter real part of first number: "))
 i1 = int(input("Enter imaginary part of first number: "))

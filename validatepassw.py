a487823edb56c8a82237502e8de82495cf0c6be3
SamJ1005 <196654975+SamJ1005@users.noreply.github.com>
2026-02-25 20:33:53 +0530

    Updated validate password print

--- validatepassw.py
+++ validatepassw.py
@@ -19,15 +19,16 @@ user_input = input("Enter a password: ")
 if validate_password(user_input):
     print("Password is valid.")
 else:
-    print("Password is invalid. Please make sure to include at least:")
-    missing_requirements = []
-    if len(user_input) < 8:
-        missing_requirements.append("a minimum length of 8 characters")
-    if not any(char.isupper() for char in user_input):
-        missing_requirements.append("at least one uppercase letter")
-    if not any(char.isdigit() for char in user_input):
-        missing_requirements.append("at least one digit")
-    if not re.search(r'[!@#$%^&*(),.?":{}|<>]', user_input):
-        missing_requirements.append("at least one special character")
+    # print("Password is invalid. Please make sure to include at least:")
+    # missing_requirements = []
+    # if len(user_input) < 8:
+    #     missing_requirements.append("a minimum length of 8 characters")
+    # if not any(char.isupper() for char in user_input):
+    #     missing_requirements.append("at least one uppercase letter")
+    # if not any(char.isdigit() for char in user_input):
+    #     missing_requirements.append("at least one digit")
+    # if not re.search(r'[!@#$%^&*(),.?":{}|<>]', user_input):
+    #     missing_requirements.append("at least one special character")
         
-    print(f"The password is invalid. Missing: {', '.join(missing_requirements)}")
+    # print(f"The password is invalid. Missing: {', '.join(missing_requirements)}")
+    print("The password is invalid.")

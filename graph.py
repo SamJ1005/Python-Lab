b79a8c179a7a01d29c1dc3b9b80cf8290a7218c5
samjac7755@gmail.com <your@email.com>
2026-03-10 21:17:13 +0530

    Updated garph

--- graph.py
+++ graph.py
@@ -3,26 +3,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Create sample data
-data = {
-    "x": [1, 2, 3, 4, 5],
-    "y": [5, 7, 6, 8, 9]
-}
+n = int(input("Enter number of values: "))
 
+x = []
+y = []
+
+print("Enter X values:")
+for i in range(n):
+    x.append(int(input()))
+
+print("Enter Y values:")
+for i in range(n):
+    y.append(int(input()))
+
+# Create DataFrame
+data = {"X": x, "Y": y}
 df = pd.DataFrame(data)
 
 # Line graph
-df.plot(x="x", y="y", kind="line", title="Line Graph")
+df.plot(x="X", y="Y", kind="line", title="Line Graph")
 plt.show()
 
 # Bar chart
-df.plot(x="x", y="y", kind="bar", title="Bar Chart")
+df.plot(x="X", y="Y", kind="bar", title="Bar Chart")
 plt.show()
 
 # Histogram
-df["y"].plot(kind="hist", title="Histogram")
+df["Y"].plot(kind="hist", title="Histogram")
 plt.show()
 
 # Scatter plot
-df.plot(x="x", y="y", kind="scatter", title="Scatter Plot")
+df.plot(x="X", y="Y", kind="scatter", title="Scatter Plot")
 plt.show()
